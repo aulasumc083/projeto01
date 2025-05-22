78a3b0fc9c1011ee8818c5905549cad04f7f5c65
aulasumc083 <aulasumc083@gmail.com>
2025-05-22 20:00:48 -0300

    Add files via upload

---
+++ arvores_01.py
@@ -0,0 +1,12 @@
+# Árvores Binárias
+
+from binarytree import Node
+
+# Criar uma árvore binária simples
+root = Node("A")    # Nível 0
+root.left = Node("B")   # Nível 1
+root.right = Node("C")  # Nível 1
+root.left.left = Node("D")  # Nível 2
+root.left.right = Node("E")  # Nível 2
+root.right.left = Node("F")  # Nível 2
+print(root) # Imprime a árvore
